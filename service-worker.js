309adbdd6cb89efda4d6535edffa99d6df9ceba0
Favour Felix <felixfavour0@gmail.com>
2022-03-10 21:32:41 +0400

    created service worker file, connected to html with javascript and added cache files

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'petstore-v1'
+const cacheFiles = [
+  'index.html',
+  'after-school-lessons.webmanifest',
+  'styles.css',
+  'images/AfterSchool.png',
+  'images/background.png',
+  'images/basketball.png',
+  'images/chess.png',
+  'images/cooking.png',
+  'images/dancing.png',
+  'images/football.png',
+  'images/icon192.png',
+  'images/icon512.png',
+  'images/karate.png',
+  'images/painting.png',
+  'images/scrabble.png',
+  'images/surfing.png',
+  'images/swimming.png'
+]
