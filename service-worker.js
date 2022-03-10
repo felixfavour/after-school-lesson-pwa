e001824df72a197363a0632e895139ffc1fc29ce
Favour Felix <felixfavour0@gmail.com>
2022-03-10 21:33:18 +0400

    cached files in cacheFiles array

--- service-worker.js
+++ service-worker.js
@@ -18,3 +18,12 @@ const cacheFiles = [
   'images/surfing.png',
   'images/swimming.png'
 ]
+
+// CACHING THE FILES IN THE SERVICE WORKER [cacheFiles] ARRAY
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install')
+  e.waitUntil (caches.open(cacheName).then((cache) => {
+    console.log('[Service Worker] Caching all files')
+    return cache.addAll(cacheFiles)
+  }))
+})
